58c9248a82065b9ba7eec50c6ef2445875f5e4a1
MrNavaStar <mr.navastar@gmail.com>
2024-10-12 20:32:34 -0600

    revert some changes

--- java.go
+++ java.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"errors"
 	"fmt"
 	"io/fs"
@@ -13,7 +12,6 @@ import (
 	"github.com/mrnavastar/assist/bytes"
 	"github.com/mrnavastar/babe/babe"
 	"github.com/urfave/cli/v2"
-	"golang.org/x/sync/errgroup"
 )
 
 const manifest = "Manifest-Version: 1.0\nMain-Class: %s\n";
@@ -61,46 +59,42 @@ func Package(mod Module) error {
 
 	var mainClass string
 
-	walkGroup, _ := errgroup.WithContext(context.Background())
-
 	err := filepath.WalkDir("build/output/", func(path string, d fs.DirEntry, err error) error {
 		if d.IsDir() {
 			return nil
 		}
 
-		walkGroup.Go(func() error {
-			data, err := os.ReadFile(path)
-			if err != nil {
-				return err
-			}
+		data, err := os.ReadFile(path)
+		if err != nil {
+			return err
+		}
 
-			member := babe.JarMember{Name: strings.TrimPrefix(path, "build/output/"), Buffer: &bytes.Buffer{Data: &data, Index: 0}}
-			c <- &member
-			class, err := member.GetAsClass()
-			if err != nil {
-				return err
-			}
+		member := babe.JarMember{Name: strings.TrimPrefix(path, "build/output/"), Buffer: &bytes.Buffer{Data: &data, Index: 0}}
+		c <- &member
+		class, err := member.GetAsClass()
+		if err != nil {
+			return err
+		}
 
-			if class.HasMainMethod() {
-				if mainClass != "" {
-					return errors.New("jar has too many main method declarations - only one allowed")
-				}
-				manifestBytes := []byte(fmt.Sprintf(manifest, strings.ReplaceAll(mainClass, "/", ".")))
-				c <- &babe.JarMember{Name: "META-INF/MANIFEST.MF", Buffer: &bytes.Buffer{Data: &manifestBytes, Index: 0}}
+		if class.HasMainMethod() {
+			if mainClass != "" {
+				return errors.New("jar has too many main method declarations - only one allowed")
 			}
-			return nil
-
-		})
+			mainClass = class.GetClassName()
+		}
 		return nil
 	})
 	if err != nil {
 		return err
 	}
 
-	if err := walkGroup.Wait(); err != nil {
-		return err
-	}
+	manifestBytes := []byte(fmt.Sprintf(manifest, strings.ReplaceAll(mainClass, "/", ".")))
+	c <- &babe.JarMember{Name: "META-INF/MANIFEST.MF", Buffer: &bytes.Buffer{Data: &manifestBytes, Index: 0}}
 	close(c)
 
-	return group.Wait()
+	if err := group.Wait(); err != nil {
+		return err
+	}
+
+	return nil 
 }
